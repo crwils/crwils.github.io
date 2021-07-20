dff712b06bfb64a12dd3b2cd5cd3ee655a2116df
Craig <craigwilsonmusic@gmail.com>
2021-07-20 21:08:16 +0100

    added basic header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,15 +7,39 @@ function Header(props) {
       <header className="App-header">
         <img src={myavatar} className="App-logo" alt="logo" />
         <p>
-          Hi, I'm Craig... and welcome to my portfolio!
+          Hi, I'm Craig! Welcome to my portfolio.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://soundcloud.com/craigwilsonofficial/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Music
+        </a>
+        <a
+          className="App-link"
+          href="https://twitter.com/cr_wils"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </a>
+        <a
+          className="App-link"
+          href="https://instagram.com/_crwils"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Instagram
+        </a>
+        <a
+          className="App-link"
+          href="https://linkedin.com/in/crwils"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
         </a>
       </header>
     </div>
