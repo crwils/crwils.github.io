d6dd84d8733daf6f2e6a535d1a11cb74d1cfabf9
Craig <craigwilsonmusic@gmail.com>
2021-07-26 21:08:20 +0100

    moved myprojects map function to projectitem as not required elsewhere

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 function ProjectItem(props) {
+
+    const myProjects = props.projects.projects.map((project) => {
+        return (
+          <div className="project-container">
+            <div className="project-box">
+              <div>{project.name}</div>
+              <div className="techStack small">{project.techStack}</div>
+            </div>
+          </div>
+        );
+      });
     
     return (
 
         <>
-            {props.projects}
+            {myProjects}
         </>
 
 
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,24 +1,15 @@
 import React from "react";
+
 import { ProjectItem } from ".";
 
 function ProjectList(props) {
-  const myProjects = props.projects.projects.map((project) => {
-    return (
-      <div className="project-container">
-        <div className="project-box">
-          <div>{project.name}</div>
-          <div className="techStack small">{project.techStack}</div>
-        </div>
-      </div>
-    );
-  });
-
+  
   // can change the above funciton to render a ProjectItem then return the project item below
   return (
     <div className="projects-grid max-width">
       <h3>Projects</h3>
       <div className="project-items">
-        <ProjectItem projects={myProjects} />
+        <ProjectItem projects={props.projects} />
       </div>
     </div>
   );
