8dff4d9c5469951db70aa1df5392b98cd082efb0
Craig Wilson <craigwilsonmusic@gmail.com>
2022-01-25 21:31:36 +0000

    added switch toggle and margin round project items

---
+++ src/components/DarkMode.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function DarkMode(props) {
+    return (
+        <>
+            <label class="switch">
+                <input type="checkbox"></input>
+                <span class="slider round"></span>
+            </label>
+        </>
+    );
+}
+
+export default DarkMode;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import myavatar from "../myavatar.png" 
 import "../style.css"
+import DarkMode from './DarkMode';
 
 function NavBar (props) {
 
     return (
-        <nav>
-            <div className="max-width">
-                <span className="icon-me" title="link to homepage" href=".">
-                    <a href='/'>
-                        <img type="button" src={myavatar} alt="A photograph of me" />
-                    </a>
-                </span>
-                <ul>
-                    <a className="nav-link" href='#about'>About</a>
-                    <a className="nav-link" href='#project-list'>Projects</a>
-                    <a className="nav-link" href='/contact'>Contact</a>
-                </ul>
-            </div>
-        </nav>
+        <>
+            <nav>
+                <div className="max-width">
+                    <span className="icon-me" title="link to homepage" href=".">
+                        <a href='/'>
+                            <img type="button" src={myavatar} alt="A photograph of me" />
+                        </a>
+                    </span>
+                    <DarkMode/>
+                    <ul>
+                        <a className="nav-link" href='#about'>About</a>
+                        <a className="nav-link" href='#project-list'>Projects</a>
+                        <a className="nav-link" href='/contact'>Contact</a>
+                    </ul>
+                </div>
+            </nav>
+        </>
     );
 }
 
