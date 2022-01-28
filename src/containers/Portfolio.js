0c7601a4d19e0858392f5327a3da10e0beb39f9a
Craig Wilson <craigwilsonmusic@gmail.com>
2022-01-28 11:06:40 +0000

    added pseudocode for window resize flow

--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -5,6 +5,11 @@ import "../style.css";
 
 function Portfolio(props) {
 
+  // window resize event
+  // get window size
+  // get new window size
+  // check if new size is equal to old size, if not, do something e.g. change border to dashed instead of solid
+
   return (
       <main>
         <section className="header-index">
