e08fb06203f5fdb7c8530e9a914844cb9d577838
Craig Wilson <craigwilson@gigged.ai>
2022-04-28 17:12:51 +0100

    installed tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(props) {
     return (
       <section className="container-introduction max-width">
-        <h2>
+        <h2 className="text-3xl font-bold underline">
           A detail-orientated Junior Software Developer; recent graduate of CodeClan in Edinburgh.
         </h2>
       </section>
