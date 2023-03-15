62453e8e9615ec63915e1af7081275f3a55d91fe
Craig Wilson <crwils@proton.me>
2023-03-15 20:21:27 +0000

    added temporary splash page

--- src/containers/Portfolio.tsx
+++ src/containers/Portfolio.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { Header, NavBar, ProjectList, About } from "../components";
 import "../style.css";
+import bgcrwils from "../assets/bgcrwils.png"
 
 const Portfolio = () => {
 
@@ -11,16 +12,20 @@ const Portfolio = () => {
   // check if new size is equal to old size, if not, do something e.g. change border to dashed instead of solid
 
   return (
-      <main>
-        <section className="header-index">
+    <main>
+      <div className="h-[100vh] w-full flex justify-center items-center">
+        <p className="text-[#b29c9b] font-mona bg-clip-text bg-gradient-to-l from-[#b29c9b] to-white hover:text-white transition-all duration-500 ease-in-out cursor-pointer text-8xl">crwils.com</p>
+        {/* <p className="font-mona bg-gradient-to-l from-blue-600 via-green-500 to-indigo-400 hover:text-green-500 transition-all ease-in-out duration-500 text-transparent bg-clip-text cursor-pointer text-8xl">crwils.com</p> */}
+      </div>
+      {/* <section className="header-index">
           <NavBar/>
           <Header />
         </section>
         <section>
           <About/>
           <ProjectList />
-        </section>
-      </main>
+        </section> */}
+    </main>
   );
 }
 
